7d29edc47fb218ea909a48415f3706d4fa863c28
Mefioculus <lin0ge@yandex.ru>
2023-04-17 16:56:17 +0300

    Создание библиотеки с моделью данных

---
+++ TodoModel/TodoTask.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TodoModel {
+
+    public class Task {
+        private List<Task> ChildTasks { get; set; }
+        public string Text { get; private set; }
+        public DateTime CreationDate { get; private set; }
+        public DateTime EndDate { get; private set; }
+
+
+    }
+
+}
